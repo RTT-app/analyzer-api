93ded975b1635cd5ac3e8725630ab27c153a9dc2
jordaoa <andrejordao50@gmail.com>
2023-09-15 12:06:29 -0300

    add: final route to get LDA topics.

--- src/app/controllers/analyzer.py
+++ src/app/controllers/analyzer.py
@@ -1,9 +1,10 @@
-from flask import jsonify
+from flask import request, jsonify
 from app import app, spec
 from flask_pydantic_spec import (Response, Request)
 
-from app.services.analyzer import lda_pipeline
-
+from app.services import (lda_pipeline, translate_topics)
+from app.schemas import (GenTopics, Topics)
+import pprint
 """
 Surgiu a ideia de criar um monitorador de assuntos gerais do /politcs. 
 Talvez tudo seja feito com LDA (((((((sei ainda não...)))))))
@@ -15,10 +16,13 @@ Seria uma boa ideia monitorar os sentimentos de usuarios com analise de sentimen
 # - TODO: make schema to get topics
 
 @app.post('/get-topics')
-@spec.validate(body=Request(),resp=Response(),tags=["LDA Model"])
+@spec.validate(body=Request(GenTopics),resp=Response(HTTP_200=Topics),tags=["LDA Model"])
 def gen_topics():
     """
     - Make LDA pipeline to gen the text topics.
     """
-    topics = lda_pipeline()
-    return jsonify(), 200
+    topics = lda_pipeline(request.json)
+    topics_ = translate_topics(topics)
+    topics_dict = topics_.dict()
+    
+    return jsonify(topics_dict), 20
